16750a74c5913a5b4caa2a04784a05eb0b064a90
excited-hyun <nahyun1127@naver.com>
2020-07-16 19:10:41 +0900

    Styling App with CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -32,14 +33,14 @@ class App extends React.Component {
   render() {
     const{ isLoading, movies } = this.state;    //구조분해 할당
     return (
-    <section class="container">
+    <section className="container">
       {isLoading ? (
-        <div class="loader">
-          <span class="loader__text">Loading...</span>
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
         </div>
        )
         : (
-          <div class="movies">
+          <div className="movies">
           {movies.map((movie) => {
           return( 
             <Movie 
@@ -49,6 +50,7 @@ class App extends React.Component {
               title={movie.title}
               summary={movie.summary}
               poster={movie.medium_cover_image}
+              genres={movie.genres}
             />
           );
         })}

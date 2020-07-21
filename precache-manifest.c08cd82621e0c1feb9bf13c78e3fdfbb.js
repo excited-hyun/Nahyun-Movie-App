7e61b3e953cf00535cc2d8aafcb0fc00a4a18b1b
excited-hyun <nahyun1127@naver.com>
2020-07-21 13:21:47 +0900

    Updates

---
+++ precache-manifest.c08cd82621e0c1feb9bf13c78e3fdfbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c7a664cf3098c1c47ec7bfab605d08",
+    "url": "/Nahyun-Movie-App/index.html"
+  },
+  {
+    "revision": "2600c44dd0c15a2d4f5a",
+    "url": "/Nahyun-Movie-App/static/css/main.f2129f31.chunk.css"
+  },
+  {
+    "revision": "8f5a7b91e60aba500aac",
+    "url": "/Nahyun-Movie-App/static/js/2.d83872c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nahyun-Movie-App/static/js/2.d83872c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2600c44dd0c15a2d4f5a",
+    "url": "/Nahyun-Movie-App/static/js/main.23321280.chunk.js"
+  },
+  {
+    "revision": "95252e29d7e274707632",
+    "url": "/Nahyun-Movie-App/static/js/runtime-main.a9b19733.js"
+  }
+]);
